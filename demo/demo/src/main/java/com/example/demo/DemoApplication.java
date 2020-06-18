80bb740403155ed077dfd2c197af8fdb6fbc5060
tuyc17 <t2017013579@163.com>
2020-06-18 19:23:52 +0800

    personal login page and url needless login achieved

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 
 @SpringBootApplication
@@ -15,4 +18,11 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	// 绕开安全限制
+	@Bean
+	public HttpFirewall allowUrlSemicolonHttpFirewall() {
+		StrictHttpFirewall firewall = new StrictHttpFirewall();
+		firewall.setAllowSemicolon(true);
+		return firewall;
+	}
 }
