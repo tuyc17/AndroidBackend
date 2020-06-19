3b8a9ba2d16e68b345f9feba8e9c023a9eea169b
ldegao <lshabaddon@163.com>
2020-06-20 02:56:57 +0800

    增加websocket

--- demo/demo/src/main/java/com/example/demo/dao/UserRepository.java
+++ demo/demo/src/main/java/com/example/demo/dao/UserRepository.java
@@ -3,12 +3,20 @@ package com.example.demo.dao;
 //import org.apache.ibatis.annotations.*;
 //import org.mybatis.spring.annotation.MapperScan;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.domain.User;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUserName(String userName);
+    //用学号查找
+    User findBystudentid(Integer studentid);
 
+    //通过id获取某人的信息
+    @Query(value = "select isonline, isverified, lastlogin, nickname, studentid, username from user where id = :id",nativeQuery = true)
+    List<Object[]> getRequest(@Param("id") Integer id);
 }
